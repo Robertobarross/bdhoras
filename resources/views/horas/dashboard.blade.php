07c7d294ec3b96c51d4e09a2d778d62375fc4364
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-23 20:56:16 -0300

    Create dashboard.blade.php

---
+++ resources/views/horas/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('template'){{--Extenção do arquivo template--}}
+
+@section('title', 'dbhoras'){{--Extenção do arquivo template--}}
+
+@section('content'){{--Extenção do arquivo template--}}
+
+
+
+@endsection {{--Extenção do arquivo template--}}

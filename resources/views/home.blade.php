aefebec8154ad6e79a5cfe4f1eed495f2a9bcb43
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-23 20:56:09 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,11 @@
 
 @section('content'){{--Extenção do arquivo template--}}
 
+<body {{--Imagem plano de fundo da tela inicial, e texto de boas vindas--}}
+<div style="background-image: url('/img/img-horas.jpg'); background-repeat: no-repeat; background-size: cover;">
+<h1 class="titulo-centro">Seja bem vindo(a) ao bdhoras, faça seu cadastro!
+</h1>
+</div>
 
 @endsection {{--Extenção do arquivo template--}}
 

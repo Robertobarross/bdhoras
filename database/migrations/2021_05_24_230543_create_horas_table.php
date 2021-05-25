ccd8ce312cc5390ba6dc9215a79cc75b7e96833e
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-24 23:05:52 -0300

    Create 2021_05_24_230543_create_horas_table.php

---
+++ database/migrations/2021_05_24_230543_create_horas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHorasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('horas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date("data");
+            $table->time("inicio");
+            $table->time("intervalo");
+            $table->time("retorno");
+            $table->time("final");
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('horas');
+    }
+}

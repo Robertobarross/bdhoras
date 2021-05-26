2468d1f99fb733de6429264ed6cf75961faef4f2
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-25 23:16:38 -0300

    Create show.blade.php

---
+++ resources/views/horas/show.blade.php
@@ -0,0 +1,10 @@
+@extends('template'){{--Extenção do arquivo template--}}
+
+@section('title', 'dbhoras-show'){{--Extenção do arquivo template--}}
+
+@section('content'){{--Extenção do arquivo template--}}
+
+<h1>Oi</h1>
+
+@endsection {{--Extenção do arquivo template--}}
+

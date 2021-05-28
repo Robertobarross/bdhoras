6c80999f89fcd1b95c05114289b0866a6260355b
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-27 22:17:56 -0300

    Update show.blade.php

--- resources/views/horas/show.blade.php
+++ resources/views/horas/show.blade.php
@@ -1,10 +1,16 @@
+
 @extends('template'){{--Extenção do arquivo template--}}
 
 @section('title', 'dbhoras-show'){{--Extenção do arquivo template--}}
 
 @section('content'){{--Extenção do arquivo template--}}
 
-<h1>Oi</h1>
+<p>Aula 13 até 18</p>
+
+@foreach ($horas as $hora ) {{--Declarando variáveis das informações que serão exibidas--}}
+
+<p class="texto-form"> Data: {{ $hora->data }} - Inicio: {{ $hora->inicio }} - Intervalo: {{ $hora->intervalo }} - Retorno: {{ $hora->retorno }} - Final: {{ $hora->final }}</p>
+@endforeach
+
 
 @endsection {{--Extenção do arquivo template--}}
-
